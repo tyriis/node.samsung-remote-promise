6ce07258cbdc1462eddd5b965a3186c3b2ff3b1e
Nils Mueller <nils@mueller.name>
2017-09-27 23:48:05 +0200

    Init: added files

---
+++ index.js
@@ -0,0 +1,12 @@
+
+/*!
+ *  samsung-remote-pomise
+ *  Promise wrapper for Andrei Zharov samsung-remote library (https://github.com/natalan/samsung-remote)
+ *
+ *  @author Nils Müller
+ *  @license Released under the MIT license
+ *
+ *  https://github.com/tyriis/node.samsung-remote-promise
+ */
+
+module.exports = require('./lib/samsung-remote-promise.js');
